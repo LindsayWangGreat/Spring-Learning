4f829780e9e9e86c357aae852312714435042045
Lindsay Wang <lindsay.wang01@sap.com>
2018-03-29 13:52:56 +0800

    use bean annotation

--- demo/src/main/java/com/example/demo/CDPlayerConfig.java
+++ demo/src/main/java/com/example/demo/CDPlayerConfig.java
@@ -1,9 +1,18 @@
 package com.example.demo;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class CDPlayerConfig {
+    @Bean
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+    
+    @Bean
+    public CDPlayer cdplayer(CompactDisc cd) {
+        return new CDPlayer(cd);
+    }
 }
